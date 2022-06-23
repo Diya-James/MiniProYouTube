57084ac657e48e7b7b91916ff71fecebd5d7c078
Diya-James <70277722+Diya-James@users.noreply.github.com>
2022-06-23 10:05:39 +0530

    Update Summary.js

--- src/Summary.js
+++ src/Summary.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React , {useEffect , useState}from 'react'
 import TextField from '@mui/material/TextField';
 import {useLocation} from 'react-router-dom';
 
 function Summary() {
   const location = useLocation();
+  const [sum , setsum] = useState('Loading.......')
+
+  const getData = async(result) => {
+    const response = await fetch(`https://ytvideosummariser.herokuapp.com/api?url=${result}` );
+    const data = await response.json();
+    console.log(data)
+    setsum(data.Message)
+  };
+
+  getData(location.state.text);
 
   return (
     <>
     <div>Summary</div>
-    <textarea rows="50" cols="50" id = "Transcripts" value = {location.state.sum}>
+    <textarea rows="50" cols="50" id = "Transcripts" value = {sum} defaultValue = "Loading....">
         {location.state.sum}
-    </textarea >
+    </textarea>
 
     </>
   )
