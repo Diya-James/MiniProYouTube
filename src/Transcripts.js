b0ecb2789af83541f2f7cc652da16c1df70f0e09
Diya-James <70277722+Diya-James@users.noreply.github.com>
2022-06-23 10:06:04 +0530

    Update Transcripts.js

--- src/Transcripts.js
+++ src/Transcripts.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React , {useEffect , useState}from 'react'
 import TextField from '@mui/material/TextField';
 import {useLocation} from 'react-router-dom';
 
 function Transcripts() {
   const location = useLocation();
+  const [tran , settran] = useState('Loading......')
+
+  const getData = async(result) => {
+
+    const response = await fetch(`https://ytvideosummariser.herokuapp.com/api?url=${result}` );
+    const data = await response.json();
+    console.log(data)
+    settran(data.Transcripts)
+  };
+
+  getData(location.state.text);
+
   return (
     <>
     <div>Transcripts</div>
-    <textarea rows="50" cols="50" id = "Transcripts" value = {location.state.tran}>
-    {location.state.tran}
+    <textarea rows="50" cols="50" id = "Transcripts" value = {tran}>
+    {tran}
     </textarea >
 
     </>
