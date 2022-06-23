791cf8bbbe5d52e5ff6e640328201ceb177530c5
Diya-James <70277722+Diya-James@users.noreply.github.com>
2022-06-23 10:04:52 +0530

    Update App.js
    Modified App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import Summary from './Summary';
 function App() {
   const navigate = useNavigate();
   const [text, settext] = useState('')
-  const [sum , setsum] = useState('Loading.......')
-  const [tran , settran] = useState('Loading......')
+
 
   useEffect(() => {
     
@@ -24,39 +23,32 @@ function App() {
   }
 
   
-  const handleSubmit = (e) => {
-      const result = text;
-      e.preventDefault();
-      getData(result);
-      console.log("The Result : " , result)
-  }
+  // const handleSubmit = () => {
+  //     const result = text;
+  //     getData(result);
+  //     console.log("The Result : " , result)
+  // }
 
-  const getData = async(result) => {
+  // const getData = async(result) => {
 
-    const response = await fetch(`https://ytvideosummariser.herokuapp.com/api?url=${result}` );
-    const data = await response.json();
-    console.log(data)
-    setsum(data.Message)
-    settran(data.Transcripts)
-  };
+  //   const response = await fetch(`https://ytvideosummariser.herokuapp.com/api?url=${result}` );
+  //   const data = await response.json();
+  //   console.log(data)
+  //   // setsum(data.Message)
+  //   settran(data.Transcripts)
+  // };
 
   return (
       <div className="App">
 
-      <h1>YouTube Video Summariser</h1>
-      <form onSubmit = {e => { handleSubmit(e) }}>
+      <h1>YouTube Video Summariser</h1> 
+
+      <div>
       <TextField id="outlined-basic" label="YouTube Link" variant="outlined" onChange={Input}/>
-      </form>
+      </div>
 
-      <button onClick = {() => { navigate("./summary" , { state : {sum : sum}}) } }> Summary</button>
-      <button onClick = {() => { navigate("/transcripts" , { state : {tran : tran}})}}> Transcripts</button>
-      {/* <textarea rows="50" cols="50" id = "Transcripts" value={tran}>
-        {tran}
-      </textarea >
-
-      <textarea rows="50" cols="50" id = "Message"value={sum}>
-        {sum}
-      </textarea > */}
+      <button onClick = {() => { { navigate ("./summary" , { state : {text : text}})} }  }> Summary</button>
+      <button onClick = {() => { {navigate("/transcripts" , { state : {text : text}})}} } > Transcripts</button>
 
     </div>
 
