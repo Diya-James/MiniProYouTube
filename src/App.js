b4e95a525aa66d2992920cb0f41830f764487f73
Abhinav-22 <abhinavanil9@gmail.com>
2022-07-05 21:32:01 +0530

    added styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React , {useEffect , useState}from 'react'
-import TextField from '@mui/material/TextField';
-import { BrowserRouter, Routes, Route , useNavigate  } from "react-router-dom";
-import "./App.css"
-import Summary from './Summary';
+import React, { useEffect, useState } from "react";
+import TextField from "@mui/material/TextField";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import "./App.css";
+import Summary from "./Summary";
 
 function App() {
   const navigate = useNavigate();
-  const [text, settext] = useState('')
+  const [text, settext] = useState("");
 
+  useEffect(() => {}, []);
 
-  useEffect(() => {
-    
-  }, []);
-
-  const Input = (e) => 
-  {
+  const Input = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    console.log(name , value);
-    settext(value)
-    console.log(value)
-  }
+    console.log(name, value);
+    settext(value);
+    console.log(value);
+  };
 
-  
   // const handleSubmit = () => {
   //     const result = text;
   //     getData(result);
@@ -39,20 +34,43 @@ function App() {
   // };
 
   return (
-      <div className="App">
+    <div className="App">
+      <h1 className="Heading">YouTube Video Summariser</h1>
 
-      <h1>YouTube Video Summariser</h1> 
-
-      <div>
-      <TextField id="outlined-basic" label="YouTube Link" variant="outlined" onChange={Input}/>
+      <div className="wrapper">
+        <TextField
+          className="textbox"
+          id="outlined-basic"
+          label="YouTube Link"
+          variant="outlined"
+          onChange={Input}
+        />
       </div>
 
-      <button onClick = {() => { { navigate ("./summary" , { state : {text : text}})} }  }> Summary</button>
-      <button onClick = {() => { {navigate("/transcripts" , { state : {text : text}})}} } > Transcripts</button>
-
+      <button
+        className="button"
+        onClick={() => {
+          {
+            navigate("./summary", { state: { text: text } });
+          }
+        }}
+      >
+        {" "}
+        Summary
+      </button>
+      <button
+        className="button"
+        onClick={() => {
+          {
+            navigate("/transcripts", { state: { text: text } });
+          }
+        }}
+      >
+        {" "}
+        Transcripts
+      </button>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
