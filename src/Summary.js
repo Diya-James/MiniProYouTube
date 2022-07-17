49df2d3c25acd5cda1b819c70c06342f4fcea607
Abhinav-22 <abhinavanil9@gmail.com>
2022-07-17 12:16:14 +0530

    added options

--- src/Summary.js
+++ src/Summary.js
@@ -2,15 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./Summary.css";
 import { useLocation } from "react-router-dom";
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-
-
-
-
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 function Summary() {
   const location = useLocation();
@@ -20,10 +16,9 @@ function Summary() {
   const [translation, settranslation] = useState("Loading......");
   const [rangeval, setRangeval] = useState(1.7);
   const [rangevalp, setRangevalp] = useState(2);
-  const[selects , setSelects] = useState();
+  const [selects, setSelects] = useState();
 
   const getData = async (result) => {
-
     const response = await fetch(
       `https://ytvideosummariser.herokuapp.com/api?url=${result}`
     );
@@ -34,11 +29,8 @@ function Summary() {
   };
 
   useEffect(() => {
-
     getData(location.state.text);
-
-  } ,[]);
-
+  }, []);
 
   const msg = new SpeechSynthesisUtterance();
 
@@ -46,7 +38,7 @@ function Summary() {
     if (window.speechSynthesis.speaking) {
       window.speechSynthesis.cancel();
     }
-    var voices
+    var voices;
     setTimeout(() => {
       voices = window.speechSynthesis.getVoices();
     }, 50);
@@ -65,190 +57,194 @@ function Summary() {
     window.speechSynthesis.resume();
   };
 
-
-
- 
-
-
   var link = location.state.text;
   console.log(link);
 
   const handleChange = () => {
     console.log("Clicked !!");
-  }
+  };
 
-
-  const handleChangeCode = async(e) => {
-
+  const handleChangeCode = async (e) => {
     var dict = {
+      Chinese: "zh-cn",
+      Dutch: "nl",
+      Czech: "cs",
+      French: "fr",
+      English: "en",
+      Arabic: "ar",
+      German: "de",
+      Greek: "el",
+      Gujarati: "gu",
+      Hindi: "hi",
+      Japanese: "ja",
+      Indonesian: "id",
+      Irish: "ga",
+      Italian: "it",
+      Kannada: "kn",
+      Korean: "ko",
+      Latin: "la",
+      Lithuanian: "lt",
+      Malay: "ms",
+      Malayalam: "ml",
+      Marathi: "mr",
+      Mangolian: "mn",
+      Nepali: "ne",
+      Odia: "or",
+      Persian: "fa",
+      Polish: "pl",
+      Portuguese: "pt",
+      Punjabi: "pa",
+      Romanian: "ro",
+      Russian: "ru",
+      Serbian: "sr",
+      Spanish: "es",
+      Swedish: "sv",
+      Tamil: "ta",
+      Telugu: "te",
+      Thai: "th",
+      Turkish: "tr",
+      Ukrainian: "uk",
+      Urdu: "ur",
+      Uzbek: "uz",
+      Viatnamese: "vi",
+      Albanian: "sq",
+      Bosnian: "bs",
+      Bulagarian: "bg",
+    };
 
-      "Chinese" : "zh-cn",
-      "Dutch" : "nl",
-      "Czech" : "cs",
-      "French" : "fr",
-      "English" : "en",
-      "Arabic" : "ar",
-      "German" : "de",
-      "Greek" : "el",
-      "Gujarati" : "gu",
-      "Hindi" : "hi",
-      "Japanese" : "ja",
-      "Indonesian" : "id",
-      "Irish" :"ga",
-      "Italian" : "it",
-      "Kannada" : "kn",
-      "Korean" : "ko",
-      "Latin" : "la",
-      "Lithuanian" : "lt",
-      "Malay" : "ms",
-      "Malayalam" : "ml",
-      "Marathi" : "mr",
-      "Mangolian" : "mn",
-      "Nepali" : "ne",
-      "Odia" : "or",
-      "Persian" : "fa",
-      "Polish" : "pl",
-      "Portuguese" : "pt",
-      "Punjabi" : "pa",
-      "Romanian" : "ro",
-      "Russian" : "ru",
-      "Serbian" : "sr",
-      "Spanish" : "es",
-      "Swedish" : "sv",
-      "Tamil" : "ta",
-      "Telugu" : "te",
-      "Thai" : "th",
-      "Turkish" : "tr",
-      "Ukrainian" : "uk",
-      "Urdu" : "ur",
-      "Uzbek" : "uz",
-      "Viatnamese" : "vi", 
-      "Albanian" : "sq",
-      "Bosnian" : "bs",
-      "Bulagarian" : "bg",
-  }
-
-    setSelects(e.target.value)
+    setSelects(e.target.value);
     const value = e.target.value;
     console.log(value);
-    console.log( dict[value]);
+    console.log(dict[value]);
     var code = dict[value];
-    try{
+    try {
       const response1 = await fetch(
         `https://yttranslation.herokuapp.com/api?dest=${code}&text=${sum}`
       );
       const data1 = await response1.json();
-      console.log(data1.Tranlated)
-      settranslation(data1.Translated)
+      console.log(data1.Tranlated);
+      settranslation(data1.Translated);
       setsum(data1.Tranlated);
-    }
-    catch{
+    } catch {
       console.error();
     }
-
-
-
-  }
-
+  };
 
   return (
     <React.Fragment>
-
-
-    
-      <div className = "navbar" >
-      <button className = "btn-sum" class="active"  onClick={() => navigate("/summary", { state: { text: {link} } })}>Get Summary</button>
-      <button className = "btn-trans" onClick={() => navigate("/transcripts", { state: { tran : tran  , link : link}})}>Get Transcripts</button>
+      <div className="navbar">
+        <button
+          className="btn-sum"
+          class="active"
+          onClick={() => navigate("/summary", { state: { text: { link } } })}
+        >
+          Get Summary
+        </button>
+        <button
+          className="btn-trans"
+          onClick={() =>
+            navigate("/transcripts", { state: { tran: tran, link: link } })
+          }
+        >
+          Get Transcripts
+        </button>
       </div>
-      
-      <br/><br/>
+
+      <br />
+      <br />
       <div className="sum-page">
         <div> Summary </div>
-    <div className = "content">
+        <div className="content">
+          <div className="left-flex">
+            <div className="rate-pitch">
+              <div id="rate-control">
+                <label for="rate">Rate:</label>
+                <input
+                  id="typeinp"
+                  type="range"
+                  onChange={(event) => setRangeval(event.target.value)}
+                  min="0.5"
+                  max="3"
+                  defaultValue="1.7"
+                  step="0.1"
+                />
+              </div>
+              <div id="rate-control">
+                <label for="rate">Pitch:</label>
+                <input
+                  id="typeinp"
+                  type="range"
+                  onChange={(event) => setRangevalp(event.target.value)}
+                  min="0.5"
+                  max="3"
+                  defaultValue="2"
+                  step="0.1"
+                />
+              </div>
+            </div>
 
+            <div className="speech-controller">
+              <button className="btn" onClick={() => speechHandler(msg)}>
+                <span className="btn-content">Speak</span>
+              </button>
 
-    <div className = "left-flex">
+              <button className="btn" onClick={() => speechPause()}>
+                <span className="btn-content">Pause</span>
+              </button>
 
-      <div className = "rate-pitch">
-        <div id="rate-control">
-          <label for="rate">Rate:</label>
-          <input
-            id="typeinp"
-            type="range"
-            onChange={(event) => setRangeval(event.target.value)}
-            min="0.5"
-            max="3"
-            defaultValue="1.7"
-            step="0.1"
-            />
-        </div>
-        <div id="rate-control">
-          <label for="rate">Pitch:</label>
-          <input
-            id="typeinp"
-            type="range"
-            onChange={(event) => setRangevalp(event.target.value)}
-            min="0.5"
-            max="3"
-            defaultValue="2"
-            step="0.1"
-            />
-        </div >
-      </div>
+              <button className="btn" onClick={() => speechResume()}>
+                <span className="btn-content">Resume</span>
+              </button>
+            </div>
 
-        <div className = "speech-controller">
-
-            <button className="btn" onClick={() => speechHandler(msg)}>
-              <span className="btn-content">Speak</span>
-            </button>
-
-            <button className="btn" onClick={() => speechPause()}>
-              <span className="btn-content">Pause</span>
-            </button>
-
-            <button className="btn" onClick={() => speechResume()}>
-              <span className="btn-content">Resume</span>
-            </button>
-
-            
-        </div>
-
-            <select value = {selects} onChange = {handleChangeCode}>
-
-
-            {/* "Vietnamese" : "vi",
-          "Spanish" : "es",
-          "Russian" : "ru",
-          "Portuguese" : "pt",
-          "Myanmar" : "my",
-          "Marathi" : "mr",
-          "Latin" : "la",
-          "Italian" : "it",
-          "Indonesian" : "id",
-          "Hebrew" : "he",
-          "Greek" : "el" */}
-
-
+            <select value={selects} onChange={handleChangeCode}>
               <option></option>
               <option>Chinese</option>
               <option>Dutch</option>
               <option>Czech</option>
               <option>French</option>
-              <option>Vietnamese</option>
-              <option>Spanish</option>
-              <option>Russian</option>
-              <option>Myanmar</option>
-              <option>Portuguese</option>
-              <option>Marathi</option>
-              <option>Latin</option>
-              <option>Italian</option>
-              <option>Indonesian</option>
-              <option>Hebrew</option>
+              <option>English</option>
+              <option>Arabic</option>
+              <option>German</option>
               <option>Greek</option>
-            
+              <option>Gujarati</option>
+              <option>Hindi</option>
+              <option>Japanese</option>
+              <option>Indonesian</option>
+              <option>Irish</option>
+              <option>Italian</option>
+              <option>Kannada</option>
+              <option>Korean</option>
+              <option>Latin</option>
+              <option>Lithuanian</option>
+              <option>Malay</option>
+              <option>Malayalam</option>
+              <option>Marathi</option>
+              <option>Mangolian</option>
+              <option>Nepali</option>
+              <option>Odia</option>
+              <option>Persian</option>
+              <option>Polish</option>
+              <option>Portuguese</option>
+              <option>Punjabi</option>
+              <option>Romanian</option>
+              <option>Russian</option>
+              <option>Serbian</option>
+              <option>Spanish</option>
+              <option>Swedish</option>
+              <option>Tamil</option>
+              <option>Telugu</option>
+              <option>Thai</option>
+              <option>Turkish</option>
+              <option>Ukranian</option>
+              <option>Urdu</option>
+              <option>Uzbek</option>
+              <option>Viatnamese</option>
+              <option>Albanian</option>
+              <option>Bosnian</option>
+              <option>Bulagarian</option>
             </select>
-
-        </div> 
+          </div>
           <div className="main">
             <textarea
               rows="100"
@@ -260,10 +256,9 @@ function Summary() {
               {sum}
             </textarea>
           </div>
-      
+        </div>
       </div>
-      </div>
-      </React.Fragment>
+    </React.Fragment>
   );
 }
 
