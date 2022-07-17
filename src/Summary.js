3a92707182ceb249d57e4d90b03bc0144d28cca9
Diya-James <diyaperumthottam@gmail.com>
2022-07-17 19:11:08 +0530

    Text area and css

--- src/Summary.js
+++ src/Summary.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import TextareaAutosize from 'react-textarea-autosize';
 import "./Summary.css";
 import { useLocation } from "react-router-dom";
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
@@ -8,6 +9,9 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 
+
+
+
 function Summary() {
   const location = useLocation();
   const navigate = useNavigate();
@@ -16,7 +20,8 @@ function Summary() {
   const [translation, settranslation] = useState("Loading......");
   const [rangeval, setRangeval] = useState(1.7);
   const [rangevalp, setRangevalp] = useState(2);
-  const [selects, setSelects] = useState();
+  const[selects , setSelects] = useState();
+  const[code , setcode] = useState('en');
 
   const getData = async (result) => {
     const response = await fetch(
@@ -26,6 +31,7 @@ function Summary() {
 
     setsum(data.Message);
     settran(data.Transcripts);
+    console.log("here");
   };
 
   useEffect(() => {
@@ -115,12 +121,13 @@ function Summary() {
     setSelects(e.target.value);
     const value = e.target.value;
     console.log(value);
-    console.log(dict[value]);
-    var code = dict[value];
-    try {
+    console.log( dict[value]);
+    var curr_code = dict[value];
+    try{
       const response1 = await fetch(
-        `https://yttranslation.herokuapp.com/api?dest=${code}&text=${sum}`
+        `https://yttranslation.herokuapp.com/api?dest=${curr_code}&src=${code}&text=${sum}`
       );
+      setcode(curr_code);
       const data1 = await response1.json();
       console.log(data1.Tranlated);
       settranslation(data1.Translated);
@@ -130,60 +137,62 @@ function Summary() {
     }
   };
 
-  return (
-    <React.Fragment>
-      <div className="navbar">
-        <button
-          className="btn-sum"
-          class="active"
-          onClick={() => navigate("/summary", { state: { text: { link } } })}
-        >
-          Get Summary
-        </button>
-        <button
-          className="btn-trans"
-          onClick={() =>
-            navigate("/transcripts", { state: { tran: tran, link: link } })
-          }
-        >
-          Get Transcripts
-        </button>
-      </div>
 
-      <br />
-      <br />
+
+  return (
+    <div className= "sum-page">
+
+
+    
+      <div className = "navbar" >
+      <button className = "btn-sum" class="active"  onClick={() => navigate("/summary", { state: { text: {link} } })}>Get Summary</button>
+      <button className = "btn-trans" onClick={() => navigate("/transcripts", { state: { tran : tran  , link : link}})}>Get Transcripts</button>
+      </div>
+      
       <div className="sum-page">
         <div> Summary </div>
-        <div className="content">
-          <div className="left-flex">
-            <div className="rate-pitch">
-              <div id="rate-control">
-                <label for="rate">Rate:</label>
-                <input
-                  id="typeinp"
-                  type="range"
-                  onChange={(event) => setRangeval(event.target.value)}
-                  min="0.5"
-                  max="3"
-                  defaultValue="1.7"
-                  step="0.1"
-                />
-              </div>
-              <div id="rate-control">
-                <label for="rate">Pitch:</label>
-                <input
-                  id="typeinp"
-                  type="range"
-                  onChange={(event) => setRangevalp(event.target.value)}
-                  min="0.5"
-                  max="3"
-                  defaultValue="2"
-                  step="0.1"
-                />
-              </div>
-            </div>
+      <div className = "content">
+          <div className="main">
+               <TextareaAutosize 
+                    rows="50"
+                    cols="80"
+                    id="Summary"
+                    value={sum}
+                    defaultValue="Loading...."
+                  />
+                    
+          </div>
 
-            <div className="speech-controller">
+    <div className = "speed-btn-trans">
+
+      <div className = "rate-btn">
+          <div id="rate-control">
+            <label for="rate">Rate:</label>
+            <input
+              id="typeinp"
+              type="range"
+              onChange={(event) => setRangeval(event.target.value)}
+              min="0.5"
+              max="3"
+              defaultValue="1.7"
+              step="0.1"
+              />
+          </div>
+          <div id="rate-control">
+            <label for="rate">Pitch:</label>
+            <input
+              id="typeinp"
+              type="range"
+              onChange={(event) => setRangevalp(event.target.value)}
+              min="0.5"
+              max="3"
+              defaultValue="2"
+              step="0.1"
+              />
+          </div >
+
+          <div className = "speech-controller">
+
               <button className="btn" onClick={() => speechHandler(msg)}>
                 <span className="btn-content">Speak</span>
               </button>
@@ -195,9 +204,14 @@ function Summary() {
               <button className="btn" onClick={() => speechResume()}>
                 <span className="btn-content">Resume</span>
               </button>
-            </div>
+      
+          </div>
+        </div>
 
-            <select value={selects} onChange={handleChangeCode}>
+
+          <div className="trans">
+
+            <select  value = {selects} onChange = {handleChangeCode}>
               <option></option>
               <option>Chinese</option>
               <option>Dutch</option>
@@ -207,58 +221,18 @@ function Summary() {
               <option>Arabic</option>
               <option>German</option>
               <option>Greek</option>
-              <option>Gujarati</option>
-              <option>Hindi</option>
-              <option>Japanese</option>
-              <option>Indonesian</option>
-              <option>Irish</option>
-              <option>Italian</option>
-              <option>Kannada</option>
-              <option>Korean</option>
-              <option>Latin</option>
-              <option>Lithuanian</option>
-              <option>Malay</option>
-              <option>Malayalam</option>
-              <option>Marathi</option>
-              <option>Mangolian</option>
-              <option>Nepali</option>
-              <option>Odia</option>
-              <option>Persian</option>
-              <option>Polish</option>
-              <option>Portuguese</option>
-              <option>Punjabi</option>
-              <option>Romanian</option>
-              <option>Russian</option>
-              <option>Serbian</option>
-              <option>Spanish</option>
-              <option>Swedish</option>
-              <option>Tamil</option>
-              <option>Telugu</option>
-              <option>Thai</option>
-              <option>Turkish</option>
-              <option>Ukranian</option>
-              <option>Urdu</option>
-              <option>Uzbek</option>
-              <option>Viatnamese</option>
-              <option>Albanian</option>
-              <option>Bosnian</option>
-              <option>Bulagarian</option>
+              <option>English</option>
+
             </select>
+
           </div>
-          <div className="main">
-            <textarea
-              rows="100"
-              cols="100"
-              id="Transcripts"
-              value={sum}
-              defaultValue="Loading...."
-            >
-              {sum}
-            </textarea>
-          </div>
-        </div>
+      
+        </div> 
+
+      
       </div>
-    </React.Fragment>
+      </div>
+      </div>
   );
 }
 
--- src/Transcripts.js
+++ src/Transcripts.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import TextareaAutosize from 'react-textarea-autosize';
 import "./Transcripts.css"
 
 function Transcripts() {
@@ -11,21 +12,21 @@ function Transcripts() {
   return (
     <div className = "body-trans">
 
-    <div className = "summ-trans-container">
-    <button className = "sum-btn" onClick={() => navigate("/summary", { state: { text: link } })}>Get Summary</button>
-    <button className = "trans-btn" onClick={() => navigate("/transcripts", { state: { tran: data , link:link} })}>Get Transcripts</button>
+    <div className = "navbar">
+    <button className = "btn-sum" onClick={() => navigate("/summary", { state: { text: link } })}>Get Summary</button>
+    <button className = "btn-trans" onClick={() => navigate("/transcripts", { state: { tran: data , link:link} })}>Get Transcripts</button>
     </div>
+    
     <div>Transcripts</div>
-    <div class="textarea_styles">
-            <textarea
-              rows="50"
-              cols="50"
+    <div>
+            <TextareaAutosize
+              rows="100"
+              cols="100000"
               id="Transcripts"
               value={location.state.tran}
               defaultValue="Loading...."
-            >
-              {location.state.tran}
-            </textarea>
+            />
+
           </div>
 
     </div>
